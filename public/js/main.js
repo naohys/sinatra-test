dc3740fee8090032e21ac90f9ab30bde6531650c
rin <naohys.96@gmail.com>
2015-05-21 12:40:17 +0900

    hatena-star

--- public/js/main.js
+++ public/js/main.js
@@ -6,9 +6,24 @@ $(function() {
     $.ajax({
       url:'/star?post_id=' + post_id,
       success: function(data){
-        num=$("#"+post_id).text();
-        num++;
-        $("#"+post_id).html(num);
+        //
+        // num=$("#"+post_id).text();
+        // num++;
+        // $("#"+post_id).html(num);
+
+
+        //要素を追加する li
+        //var newElement = document.createElement('li');
+        var $newElement = $("<li>");
+        //liの中のテキスト要素（★）
+        $newElement.text('○');
+
+        var $parentElement = $this.parent();
+
+        var $ulElement = $parentElement.children('ul.star-container');
+        $ulElement.append($newElement);
+
+
         },
         error: function(data){
           alert ('失敗しちゃったよ><');
